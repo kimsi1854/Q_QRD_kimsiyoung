fd33b0234dedfcd93ca8b10c721166a7af1a9d8b
kimsi1854 <103935651+kimsi1854@users.noreply.github.com>
2024-07-16 19:21:38 +0900

    Update quoridor.py

--- quoridor.py
+++ quoridor.py
@@ -2,28 +2,29 @@ import os
 from random import sample
 from time import sleep
 from board import Board
-from random_bot import Random_Bot
-from player import Player
 from Q import QLearningPlayer
 
-board = Board()
-ql_player = QLearningPlayer(board, "Q1")
-random_bot = Random_Bot(board, "B1")
-players = [ql_player, random_bot]
+# 학습 설정
 training_games = 30
+test_games = 10
 
+# 학습 단계
+board = Board()
+ql_player1 = QLearningPlayer(board, "Q1", alpha=0.1)  # 학습률을 0.1로 설정
+ql_player2 = QLearningPlayer(board, "Q2", alpha=0.05)  # 학습률을 0.05로 설정
+players = [ql_player1, ql_player2]
 
 board.quoridors(training_games, players, print_board=False)
-ql_player.save_q_table("q_table.pkl")
+ql_player1.save_q_table("q_table_q1.pkl")
+ql_player2.save_q_table("q_table_q2.pkl")
 
-# 테스트
+# 테스트 단계
 board = Board()
-test_ql_player = QLearningPlayer(board, "Q1")
-test_ql_player.load_q_table("q_table.pkl")
-random_bot = Random_Bot(board, "B1")
-players = [test_ql_player, random_bot]
-test_games = 10
+test_ql_player1 = QLearningPlayer(board, "Q1", alpha=0.1)
+test_ql_player2 = QLearningPlayer(board, "Q2", alpha=0.05)
+test_ql_player1.load_q_table("q_table_q1.pkl")
+test_ql_player2.load_q_table("q_table_q2.pkl")
+players = [test_ql_player1, test_ql_player2]
 
 board.quoridors(test_games, players, print_board=True)
 print("[{}  {} : {}  {}]".format(players[0].name, players[0].score, players[1].score, players[1].name))
-
